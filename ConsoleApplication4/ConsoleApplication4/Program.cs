7cb2170644a45bb8180771e84559b755f5142cd7
MJON27 <43753198+MJON27@users.noreply.github.com>
2018-10-11 10:16:51 +0100

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int P = 20;
+            int[] list = { 1, 5, 5, 2 };
+            int size = list.Length;
+            for (int i = 0; i < size; i++){
+                sum += list[i];
+            }
+                if (sum >= P){
+                    Console.WriteLine("True");
+                }
+                else{
+                    Console.WriteLine("False");
+                }
+                
+
+               
+            
+
+           }
+     }
+}
+
